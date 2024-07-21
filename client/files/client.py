41f1031c22458ab489e1195fd3af4b0c3428c78d
Patrickera <patrick.araujo@ufv.br>
2024-07-20 21:21:18 -0300

    criação da lógica de pings, estou trabalhano no server.py

--- client/files/client.py
+++ client/files/client.py
@@ -1,37 +1,21 @@
 import socket
+import sys
 
-def udp_client(message, hostname):
-    try:
-        # Cria um socket UDP
-        a_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        
-        # Converte a mensagem para bytes
-        message_bytes = message.encode()
-        
-        # HOSTNAME == IP
-        server_address = (hostname, 8000)
-        
-        # Envia a mensagem para o servidor
-        a_socket.sendto(message_bytes, server_address)
-        
-        # Prepara um buffer para a resposta
-        buffer_size = 1000
-        buffer = bytearray(buffer_size)
-        
-        # Recebe a resposta do servidor
-        reply, _ = a_socket.recvfrom(buffer_size)
-        
-        print("Reply:", reply.decode())
-    except socket.error as e:
-        print("Socket error:", e)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        # Fecha o socket
-        a_socket.close()
+def start_client(message, hostname):
+    server_port = 8020
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((hostname, server_port))
 
-if name == "main":
-    import sys
-    #coloca aqui o IP do servidor
-    data = ["ola mundo", "0.0.0.1"]
-    udp_client(data[0], data[1])
+
+    client_socket.sendall(message.encode())
+    data = client_socket.recv(1024).decode()
+    print("Received:", data)
+
+if __name__ == "__main__":
+    while True:
+        message = input("enter message: ")
+        start_client(message, "server")
+    #if len(sys.argv) != 3:
+    #    print("Usage: python client.py <message> <hostname>")
+    #    sys.exit(1)
+    #start_client(sys.argv[1], sys.argv[2])
