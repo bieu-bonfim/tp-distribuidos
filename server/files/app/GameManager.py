0cdce8ec6d381a4f8b6566f450db666e79cbd673
bieu-bonfim <gabrielmmbonfim@gmail.com>
2024-08-11 16:54:13 -0300

    add: game manager starting

--- server/files/app/GameManager.py
+++ server/files/app/GameManager.py
@@ -1,5 +1,8 @@
 from controller.UserController import UserController
+from random import randint
 
 class GameManager:
     def __init__(self, conn):
-        self.userController = UserController(conn)  
+        self.lobby = None
+        self.round = 0
+        self.current_player = randint(0, 3)
