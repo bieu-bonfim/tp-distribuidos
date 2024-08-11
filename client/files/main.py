c9e93d3d2517bb71897300c94fa724b9ff249a35
Patrickera <patrick.araujo@ufv.br>
2024-08-10 23:05:18 -0300

    junção login/server

--- client/files/main.py
+++ client/files/main.py
@@ -1,9 +1,35 @@
-from client import Client
+import socket
 import threading
+import time
+import client
+import json
+import login
+import arcade
+import arcade.gui
+from arcade import load_texture
+from arcade.gui import UIManager
+from arcade.gui.widgets import UITextArea, UIInputText, UITexturePane
+from client import Client
+
+# Screen title and size
+SCREEN_WIDTH = 1412
+SCREEN_HEIGHT = 868
+SCREEN_TITLE = "Cryptids: The Conspiracy"
+BASE_MARGIN = 30
+
+time.sleep(5)
 
 def main():
+
     client = Client()
     threading.Thread(target=client.startClient).start()
-    
-if __name__ == '__main__':
+
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = login.Login(client)
+    window.show_view(start_view)
+    arcade.run()
+    #return s
+
+
+if __name__ == "__main__":
     main()
