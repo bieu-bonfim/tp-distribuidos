52f1ed0168238a95fa3c5524a90fb8461923e085
bieu-bonfim <gabrielmmbonfim@gmail.com>
2024-08-11 00:18:11 -0300

    fix: controller instantiation

--- server/files/app/InventoryManager.py
+++ server/files/app/InventoryManager.py
@@ -1,14 +1,16 @@
 from controller.UserCardsController import UserCardsController
+from controller.DeckController import DeckController
 
 class InventoryManager:
   def __init__(self, conn):
     self.conn = conn
     self.userCardsController = UserCardsController(conn)
+    self.deckController = DeckController(conn)
     
   def showUserInventory(self, user_id):
     all_cards = self.userCardsController.getCardByUser(user_id)
     card_obj = [{"card_name": card[0]} for card in all_cards]
-    all_decks = self.userCardsController.getDeckByUser(user_id)
+    all_decks = self.deckController.getDeckByUser(user_id)
     decks_obj = []
     
     return {
