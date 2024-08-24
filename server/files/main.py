e13139297819a2394be2a3a1291ee87116364287
Patrickera <patrick.araujo@ufv.br>
2024-08-23 21:58:22 -0300

    Bap primordial

--- server/files/main.py
+++ server/files/main.py
@@ -1,10 +1,8 @@
-from network.SocketServer import SocketServer
-import threading
+from network.PyroServer import PyroServer
 
 def main():
-    
-    server = SocketServer()
-    threading.Thread(target=server.serverStart).start()
+    server = PyroServer()
+    server.startServer(ns_host='127.0.0.1', ns_port=8020)
         
 if __name__ == '__main__':
     main()
---
+++ server/files/network/PyroServer.py
@@ -0,0 +1,27 @@
+import Pyro5.api
+from app.LobbyManager import LobbyManager
+from network.Client import Client
+
+
+@Pyro5.api.expose
+class PyroServer:
+    def __init__(self):
+        self.clients = list()
+        self.lobbyManager = LobbyManager()
+        self.clients = list()
+        print('Pyro5 Server iniciado!')
+
+    def startServer(self, ns_host='127.0.0.1', ns_port=8020):
+        daemon = Pyro5.api.Daemon()
+
+        try:
+            ns = Pyro5.api.locate_ns(host=ns_host, port=ns_port)
+        except Pyro5.errors.NamingError as e:
+            print(f"Could not locate the NameServer: {e}")
+            return
+
+        uri = daemon.register(self)
+        ns.register("cryptids.server", uri)
+
+        print("Pyro5 Server started! Server is registered with URI:", uri)
+        daemon.requestLoop()
